bdfd9645e0383742bdc5879bb087a62f516ab83f
SantiRendon <xantirendonc@gmail.com>
2024-06-17 16:13:58 -0500

    💄 Add navigation for SCREEN 2 in +page.svelte

--- src/pip-boy-frontend/src/routes/+page.svelte
+++ src/pip-boy-frontend/src/routes/+page.svelte
@@ -16,9 +16,13 @@
 <div class="retro-container scanline-effect">
   <header>
       <h1>jajaja</h1>
+      <!-- svelte-ignore a11y-no-redundant-roles -->
       <nav role="navigation">
           <ul>
               <li><a href="#screen-1">SCREEN 1</a></li>
+          </ul>
+          <ul>
+              <li><a href="#screen-2">SCREEN 2</a></li>
           </ul>
       </nav>
   </header>
@@ -27,6 +31,19 @@
           <article>
               <header>
                   <h2>SCREEN 1</h2>
+              </header>
+              <div class="content">
+                  Lorem ipsum...
+              </div>
+              <figure>
+                  <img src="https://tiermaker.com/images/template_images/2022/15600753/fallout-new-vegas-perk-tier-list-all-dlc-and-correct-perks-15600753/slayerfnv.png" alt="alt text for illustration">
+              </figure>
+          </article>
+      </section>
+      <section id="screen-2">
+          <article>
+              <header>
+                  <h2>SCREEN 2</h2>
               </header>
               <div class="content">
                   Lorem ipsum...
